50324e4c5e1443403eda794afbb66f22ae5c11d9
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-07-09 17:45:48 +0200

    Added cors policy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import Router from "./api/routes.js";
 import express from "express";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({
     extended: true
 }));
-
 app.use("/api/", Router);
 
 app.listen(PORT, () => console.log(`[EXPRESS] Starting server on port: ${PORT}.`));
